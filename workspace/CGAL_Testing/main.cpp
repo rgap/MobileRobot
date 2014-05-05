0731281fa451197e3d1d0d9a7967c9735d5baaf9
rgap <rgap449@gmail.com>
2014-05-05 17:26:47 -0500

    other commit

--- workspace/CGAL_Testing/main.cpp
+++ workspace/CGAL_Testing/main.cpp
@@ -1,45 +1,39 @@
-#include <CGAL/Cartesian.h>
-#include <CGAL/MP_Float.h>
-#include <CGAL/Quotient.h>
-#include <CGAL/Arr_segment_traits_2.h>
-#include <CGAL/Sweep_line_2_algorithms.h>
-#include <list>
-
-typedef CGAL::Quotient<CGAL::MP_Float> NT;
-typedef CGAL::Cartesian<NT> Kernel;
-typedef Kernel::Point_2 Point_2;
-typedef CGAL::Arr_segment_traits_2<Kernel> Traits_2;
-typedef Traits_2::Curve_2 Segment_2;
-
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Mesh_triangulation_3.h>
+#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
+#include <CGAL/Mesh_criteria_3.h>
+#include <CGAL/Implicit_mesh_domain_3.h>
+#include <CGAL/make_mesh_3.h>
+// Domain
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::FT FT;
+typedef K::Point_3 Point;
+typedef FT ( Function)(const Point&);
+typedef CGAL::Implicit_mesh_domain_3<Function, K> Mesh_domain;
+// Triangulation
+typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
+typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
+// Criteria
+typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
+// To avoid verbose function and named parameters call
+using namespace CGAL::parameters;
+// Function
+FT sphere_function(const Point& p) {
+	return CGAL::squared_distance(p, Point(CGAL::ORIGIN)) - 1;
+}
 int main() {
-	// Construct the input segments.
-	Segment_2 segments[] = { Segment_2(Point_2(1, 5), Point_2(8, 5)),
-			Segment_2(Point_2(1, 1), Point_2(8, 8)), Segment_2(Point_2(3, 1),
-					Point_2(3, 8)), Segment_2(Point_2(8, 5), Point_2(8, 8)) };
+	// Domain (Warning: Sphere_3 constructor uses squared radius !)
+	Mesh_domain domain(sphere_function, K::Sphere_3(CGAL::ORIGIN, 2.));
+	// Mesh criteria
+	Mesh_criteria
+			criteria(facet_angle = 30, facet_size = 0.1,
+					facet_distance = 0.025, cell_radius_edge_ratio = 2,
+					cell_size = 0.1);
 
-	// Compute all intersection points.
-	std::list<Point_2> pts;
-
-	CGAL::compute_intersection_points(segments, segments + 4,
-			std::back_inserter(pts));
-
-	// Print the result.
-	std::cout << "Found " << pts.size() << " intersection points: "
-			<< std::endl;
-	std::copy(pts.begin(), pts.end(),
-			std::ostream_iterator<Point_2>(std::cout, "\n"));
-
-	// Compute the non-intersecting sub-segments induced by the input segments.
-	std::list<Segment_2> sub_segs;
-
-	CGAL::compute_subcurves(segments, segments + 4,
-			std::back_inserter(sub_segs));
-
-	std::cout << "Found " << sub_segs.size()
-			<< " interior-disjoint sub-segments." << std::endl;
-
-	CGAL_assertion(CGAL::do_curves_intersect(segments, segments + 4));
-
+	// Mesh generation
+	C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
+	// Output
+	std::ofstream medit_file("out.mesh");
+	c3t3.output_to_medit(medit_file);
 	return 0;
 }
-
--- workspace/MeshTesting/Debug/objects.mk
+++ workspace/MeshTesting/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lCGAL -lpcl_apps -lpcl_common -lpcl_features -lpcl_filters -lpcl_io -lpcl_kdtree -lpcl_keypoints -lpcl_octree -lpcl_registration -lpcl_sample_consensus -lpcl_search -lpcl_segmentation -lpcl_surface -lpcl_tracking -lpcl_visualization -lflann -lvtkRendering -lvtkGraphics -lvtkIO -lvtkFiltering -lvtkCommon -lvtksys -lboost_thread -lboost_system -lboost_filesystem -lboost_graph_parallel -ldl -lpthread
+LIBS := -lCGAL -lpcl_apps -lpcl_common -lpcl_features -lpcl_filters -lpcl_io -lpcl_kdtree -lpcl_keypoints -lpcl_octree -lpcl_registration -lpcl_sample_consensus -lpcl_search -lpcl_segmentation -lpcl_surface -lpcl_tracking -lpcl_visualization -lflann -lvtkRendering -lvtkGraphics -lvtkIO -lvtkFiltering -lvtkCommon -lvtksys -lboost_thread -lboost_system -lboost_filesystem -lboost_graph_parallel -ldl -lpthread -lgmp -lmpfr
 
