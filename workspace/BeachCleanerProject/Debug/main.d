0a9734f5b39236b4e2fe17f5f229d3310fba53dd
rgap <rgap449@gmail.com>
2014-05-13 23:38:16 -0500

    other

--- workspace/BeachCleanerProject/Debug/main.d
+++ workspace/BeachCleanerProject/Debug/main.d
@@ -1,290 +1,66 @@
-main.d: ../main.cpp ../globals.h ../typedefs.h ../Capture/DeviceCapture.h \
- ../RobotPlanning/RobotFSM.h \
- ../RobotPlanning/../DataTransfer/SerialDevice.h ../RobotVision.h \
- ../ModelParameters.h ../2D_Processing/PreProcess.h \
- ../2D_Processing/Retinex.h ../2D_Processing/CanRecognition.h \
- ../3D_Processing/DepthSensing.h ../3D_Processing/PointCloud.h \
- /usr/include/pcl-1.7/pcl/ModelCoefficients.h \
- /usr/include/pcl-1.7/pcl/PCLHeader.h \
- /usr/include/pcl-1.7/pcl/pcl_macros.h \
- /usr/include/pcl-1.7/pcl/pcl_config.h \
- /usr/include/pcl-1.7/pcl/io/pcd_io.h \
- /usr/include/pcl-1.7/pcl/point_cloud.h \
- /usr/include/pcl-1.7/pcl/io/file_io.h \
- /usr/include/pcl-1.7/pcl/common/io.h /usr/include/pcl-1.7/pcl/pcl_base.h \
- /usr/include/pcl-1.7/pcl/conversions.h \
- /usr/include/pcl-1.7/pcl/common/impl/io.hpp \
- /usr/include/pcl-1.7/pcl/common/concatenate.h \
- /usr/include/pcl-1.7/pcl/point_types.h \
- /usr/include/pcl-1.7/pcl/register_point_struct.h \
- /usr/include/pcl-1.7/pcl/io/boost.h \
- /usr/include/pcl-1.7/pcl/io/impl/pcd_io.hpp \
- /usr/include/pcl-1.7/pcl/io/lzf.h \
- /usr/include/pcl-1.7/pcl/visualization/pcl_visualizer.h \
- /usr/include/pcl-1.7/pcl/correspondence.h \
- /usr/include/pcl-1.7/pcl/PolygonMesh.h \
- /usr/include/pcl-1.7/pcl/Vertices.h \
- /usr/include/pcl-1.7/pcl/visualization/common/actor_map.h \
- /usr/include/pcl-1.7/pcl/visualization/point_cloud_handlers.h \
- /usr/include/pcl-1.7/pcl/visualization/point_cloud_geometry_handlers.h \
- /usr/include/pcl-1.7/pcl/visualization/point_cloud_color_handlers.h \
- /usr/include/vtk-5.8/vtkLODActor.h \
- /usr/include/pcl-1.7/pcl/visualization/boost.h \
- /usr/include/pcl-1.7/pcl/visualization/point_picking_event.h \
- /usr/include/vtk-5.8/vtkCommand.h \
- /usr/include/pcl-1.7/pcl/visualization/area_picking_event.h \
- /usr/include/pcl-1.7/pcl/visualization/interactor_style.h \
- /usr/include/pcl-1.7/pcl/visualization/common/ren_win_interact_map.h \
- /usr/include/pcl-1.7/pcl/visualization/keyboard_event.h \
- /usr/include/pcl-1.7/pcl/visualization/mouse_event.h \
- /usr/include/vtk-5.8/vtkInteractorStyleRubberBandPick.h \
- /usr/include/pcl-1.7/pcl/visualization/impl/pcl_visualizer.hpp \
- /usr/include/vtk-5.8/vtkCellArray.h \
- /usr/include/vtk-5.8/vtkLeaderActor2D.h \
- /usr/include/vtk-5.8/vtkVectorText.h \
- /usr/include/vtk-5.8/vtkAlgorithmOutput.h \
- /usr/include/vtk-5.8/vtkFollower.h \
- /usr/include/vtk-5.8/vtkSphereSource.h \
- /usr/include/vtk-5.8/vtkProperty2D.h \
- /usr/include/vtk-5.8/vtkDataSetSurfaceFilter.h \
- /usr/include/vtk-5.8/vtkPointData.h \
- /usr/include/vtk-5.8/vtkPolyDataMapper.h \
- /usr/include/vtk-5.8/vtkProperty.h /usr/include/vtk-5.8/vtkMapper.h \
- /usr/include/vtk-5.8/vtkCellData.h \
- /usr/include/vtk-5.8/vtkDataSetMapper.h \
- /usr/include/vtk-5.8/vtkRenderer.h \
- /usr/include/vtk-5.8/vtkRendererCollection.h \
- /usr/include/vtk-5.8/vtkAppendPolyData.h \
- /usr/include/vtk-5.8/vtkTextProperty.h \
- /usr/include/pcl-1.7/pcl/visualization/common/shapes.h \
- /usr/include/pcl-1.7/pcl/geometry/planar_polygon.h \
- /usr/include/pcl-1.7/pcl/common/eigen.h \
- /usr/include/pcl-1.7/pcl/visualization/common/impl/shapes.hpp \
- /usr/include/vtk-5.8/vtkPolygon.h \
- /usr/include/vtk-5.8/vtkUnstructuredGrid.h ../3D_Processing/../globals.h \
- ../3D_Processing/PointCloudRGB.h ../3D_Processing/PointCloudGrayscale.h \
- ../3D_Processing/SACMethod/SAC_MethodXYZ.h \
- /usr/include/pcl-1.7/pcl/filters/extract_indices.h \
- /usr/include/pcl-1.7/pcl/filters/filter_indices.h \
- /usr/include/pcl-1.7/pcl/filters/filter.h \
- /usr/include/pcl-1.7/pcl/filters/boost.h \
- /usr/include/pcl-1.7/pcl/common/common_headers.h \
- /usr/include/pcl-1.7/pcl/common/common.h \
- /usr/include/pcl-1.7/pcl/common/impl/common.hpp \
- /usr/include/pcl-1.7/pcl/common/norms.h \
- /usr/include/pcl-1.7/pcl/common/impl/norms.hpp \
- /usr/include/pcl-1.7/pcl/common/angles.h \
- /usr/include/pcl-1.7/pcl/common/impl/angles.hpp \
- /usr/include/pcl-1.7/pcl/common/time.h \
- /usr/include/pcl-1.7/pcl/common/file_io.h \
- /usr/include/pcl-1.7/pcl/common/impl/file_io.hpp \
- /usr/include/pcl-1.7/pcl/sample_consensus/method_types.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/model_types.h \
- /usr/include/pcl-1.7/pcl/segmentation/sac_segmentation.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/sac.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/boost.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/sac_model.h \
- /usr/include/pcl-1.7/pcl/search/search.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/ransac.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/sac_model_plane.h \
- /usr/include/pcl-1.7/pcl/sample_consensus/sac_model_sphere.h
+main.d: ../main.cpp /usr/include/ni/XnCppWrapper.h \
+ /usr/include/ni/XnOpenNI.h /usr/include/ni/XnTypes.h \
+ /usr/include/ni/XnStatus.h /usr/include/ni/XnPlatform.h \
+ /usr/include/ni/Linux-x86/XnPlatformLinux-x86.h /usr/include/ni/XnOS.h \
+ /usr/include/ni/XnMacros.h /usr/include/ni/XnStatusCodes.h \
+ /usr/include/ni/XnStatus.h /usr/include/ni/XnOSStrings.h \
+ /usr/include/ni/XnOSMemory.h /usr/include/ni/Linux-x86/XnOSLinux-x86.h \
+ /usr/include/ni/XnContext.h /usr/include/ni/XnTypes.h \
+ /usr/include/ni/XnQueries.h /usr/include/ni/XnPrdNodeInfoList.h \
+ /usr/include/ni/XnLicensing.h /usr/include/ni/XnUtils.h \
+ /usr/include/ni/XnModuleInterface.h /usr/include/ni/XnPrdNodeInfo.h \
+ /usr/include/ni/XnQueries.h /usr/include/ni/XnPrdNode.h \
+ /usr/include/ni/XnEnumerationErrors.h /usr/include/ni/XnVersion.h \
+ /usr/include/ni/XnCodecIDs.h
 
-../globals.h:
+/usr/include/ni/XnCppWrapper.h:
 
-../typedefs.h:
+/usr/include/ni/XnOpenNI.h:
 
-../Capture/DeviceCapture.h:
+/usr/include/ni/XnTypes.h:
 
-../RobotPlanning/RobotFSM.h:
+/usr/include/ni/XnStatus.h:
 
-../RobotPlanning/../DataTransfer/SerialDevice.h:
+/usr/include/ni/XnPlatform.h:
 
-../RobotVision.h:
+/usr/include/ni/Linux-x86/XnPlatformLinux-x86.h:
 
-../ModelParameters.h:
+/usr/include/ni/XnOS.h:
 
-../2D_Processing/PreProcess.h:
+/usr/include/ni/XnMacros.h:
 
-../2D_Processing/Retinex.h:
+/usr/include/ni/XnStatusCodes.h:
 
-../2D_Processing/CanRecognition.h:
+/usr/include/ni/XnStatus.h:
 
-../3D_Processing/DepthSensing.h:
+/usr/include/ni/XnOSStrings.h:
 
-../3D_Processing/PointCloud.h:
+/usr/include/ni/XnOSMemory.h:
 
-/usr/include/pcl-1.7/pcl/ModelCoefficients.h:
+/usr/include/ni/Linux-x86/XnOSLinux-x86.h:
 
-/usr/include/pcl-1.7/pcl/PCLHeader.h:
+/usr/include/ni/XnContext.h:
 
-/usr/include/pcl-1.7/pcl/pcl_macros.h:
+/usr/include/ni/XnTypes.h:
 
-/usr/include/pcl-1.7/pcl/pcl_config.h:
+/usr/include/ni/XnQueries.h:
 
-/usr/include/pcl-1.7/pcl/io/pcd_io.h:
+/usr/include/ni/XnPrdNodeInfoList.h:
 
-/usr/include/pcl-1.7/pcl/point_cloud.h:
+/usr/include/ni/XnLicensing.h:
 
-/usr/include/pcl-1.7/pcl/io/file_io.h:
+/usr/include/ni/XnUtils.h:
 
-/usr/include/pcl-1.7/pcl/common/io.h:
+/usr/include/ni/XnModuleInterface.h:
 
-/usr/include/pcl-1.7/pcl/pcl_base.h:
+/usr/include/ni/XnPrdNodeInfo.h:
 
-/usr/include/pcl-1.7/pcl/conversions.h:
+/usr/include/ni/XnQueries.h:
 
-/usr/include/pcl-1.7/pcl/common/impl/io.hpp:
+/usr/include/ni/XnPrdNode.h:
 
-/usr/include/pcl-1.7/pcl/common/concatenate.h:
+/usr/include/ni/XnEnumerationErrors.h:
 
-/usr/include/pcl-1.7/pcl/point_types.h:
+/usr/include/ni/XnVersion.h:
 
-/usr/include/pcl-1.7/pcl/register_point_struct.h:
-
-/usr/include/pcl-1.7/pcl/io/boost.h:
-
-/usr/include/pcl-1.7/pcl/io/impl/pcd_io.hpp:
-
-/usr/include/pcl-1.7/pcl/io/lzf.h:
-
-/usr/include/pcl-1.7/pcl/visualization/pcl_visualizer.h:
-
-/usr/include/pcl-1.7/pcl/correspondence.h:
-
-/usr/include/pcl-1.7/pcl/PolygonMesh.h:
-
-/usr/include/pcl-1.7/pcl/Vertices.h:
-
-/usr/include/pcl-1.7/pcl/visualization/common/actor_map.h:
-
-/usr/include/pcl-1.7/pcl/visualization/point_cloud_handlers.h:
-
-/usr/include/pcl-1.7/pcl/visualization/point_cloud_geometry_handlers.h:
-
-/usr/include/pcl-1.7/pcl/visualization/point_cloud_color_handlers.h:
-
-/usr/include/vtk-5.8/vtkLODActor.h:
-
-/usr/include/pcl-1.7/pcl/visualization/boost.h:
-
-/usr/include/pcl-1.7/pcl/visualization/point_picking_event.h:
-
-/usr/include/vtk-5.8/vtkCommand.h:
-
-/usr/include/pcl-1.7/pcl/visualization/area_picking_event.h:
-
-/usr/include/pcl-1.7/pcl/visualization/interactor_style.h:
-
-/usr/include/pcl-1.7/pcl/visualization/common/ren_win_interact_map.h:
-
-/usr/include/pcl-1.7/pcl/visualization/keyboard_event.h:
-
-/usr/include/pcl-1.7/pcl/visualization/mouse_event.h:
-
-/usr/include/vtk-5.8/vtkInteractorStyleRubberBandPick.h:
-
-/usr/include/pcl-1.7/pcl/visualization/impl/pcl_visualizer.hpp:
-
-/usr/include/vtk-5.8/vtkCellArray.h:
-
-/usr/include/vtk-5.8/vtkLeaderActor2D.h:
-
-/usr/include/vtk-5.8/vtkVectorText.h:
-
-/usr/include/vtk-5.8/vtkAlgorithmOutput.h:
-
-/usr/include/vtk-5.8/vtkFollower.h:
-
-/usr/include/vtk-5.8/vtkSphereSource.h:
-
-/usr/include/vtk-5.8/vtkProperty2D.h:
-
-/usr/include/vtk-5.8/vtkDataSetSurfaceFilter.h:
-
-/usr/include/vtk-5.8/vtkPointData.h:
-
-/usr/include/vtk-5.8/vtkPolyDataMapper.h:
-
-/usr/include/vtk-5.8/vtkProperty.h:
-
-/usr/include/vtk-5.8/vtkMapper.h:
-
-/usr/include/vtk-5.8/vtkCellData.h:
-
-/usr/include/vtk-5.8/vtkDataSetMapper.h:
-
-/usr/include/vtk-5.8/vtkRenderer.h:
-
-/usr/include/vtk-5.8/vtkRendererCollection.h:
-
-/usr/include/vtk-5.8/vtkAppendPolyData.h:
-
-/usr/include/vtk-5.8/vtkTextProperty.h:
-
-/usr/include/pcl-1.7/pcl/visualization/common/shapes.h:
-
-/usr/include/pcl-1.7/pcl/geometry/planar_polygon.h:
-
-/usr/include/pcl-1.7/pcl/common/eigen.h:
-
-/usr/include/pcl-1.7/pcl/visualization/common/impl/shapes.hpp:
-
-/usr/include/vtk-5.8/vtkPolygon.h:
-
-/usr/include/vtk-5.8/vtkUnstructuredGrid.h:
-
-../3D_Processing/../globals.h:
-
-../3D_Processing/PointCloudRGB.h:
-
-../3D_Processing/PointCloudGrayscale.h:
-
-../3D_Processing/SACMethod/SAC_MethodXYZ.h:
-
-/usr/include/pcl-1.7/pcl/filters/extract_indices.h:
-
-/usr/include/pcl-1.7/pcl/filters/filter_indices.h:
-
-/usr/include/pcl-1.7/pcl/filters/filter.h:
-
-/usr/include/pcl-1.7/pcl/filters/boost.h:
-
-/usr/include/pcl-1.7/pcl/common/common_headers.h:
-
-/usr/include/pcl-1.7/pcl/common/common.h:
-
-/usr/include/pcl-1.7/pcl/common/impl/common.hpp:
-
-/usr/include/pcl-1.7/pcl/common/norms.h:
-
-/usr/include/pcl-1.7/pcl/common/impl/norms.hpp:
-
-/usr/include/pcl-1.7/pcl/common/angles.h:
-
-/usr/include/pcl-1.7/pcl/common/impl/angles.hpp:
-
-/usr/include/pcl-1.7/pcl/common/time.h:
-
-/usr/include/pcl-1.7/pcl/common/file_io.h:
-
-/usr/include/pcl-1.7/pcl/common/impl/file_io.hpp:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/method_types.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/model_types.h:
-
-/usr/include/pcl-1.7/pcl/segmentation/sac_segmentation.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/sac.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/boost.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/sac_model.h:
-
-/usr/include/pcl-1.7/pcl/search/search.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/ransac.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/sac_model_plane.h:
-
-/usr/include/pcl-1.7/pcl/sample_consensus/sac_model_sphere.h:
+/usr/include/ni/XnCodecIDs.h:
